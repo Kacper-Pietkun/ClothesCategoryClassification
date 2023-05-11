4f5dd0d1fb9afa32f62e36cce5f5c3d0e9345341
Kacper-Pietkun <kacper.pietkun00@gmail.com>
2023-05-11 19:49:56 +0200

    Minor changes in CNN class

--- src/classes/baisc_cnn.py
+++ src/classes/baisc_cnn.py
@@ -11,13 +11,14 @@ class CNN(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(44944, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 5)
+        self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = th.flatten(x, 1)  # flatten all dimensions except batch
+        x = th.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
