6434195cc239cb107926b7f1d8c86f7f86819b93
Kacper-Pietkun <kacper.pietkun00@gmail.com>
2023-05-16 21:54:44 +0200

    Generalize model saver for other metrics

--- src/classes/best_model_saver.py
+++ src/classes/best_model_saver.py
@@ -1,14 +1,15 @@
+import math
 import torch as th
 
 
 class BestModelSaver:
     def __init__(self, save_path):
-        self.best_val_accuracy = -1
+        self.best_metric = -math.inf
         self.save_path = save_path
 
-    def __call__(self, val_accuracy, model, optimizer, epoch):
-        if val_accuracy > self.best_val_accuracy:
-            self.best_val_accuracy = val_accuracy
+    def __call__(self, metric, model, optimizer, epoch):
+        if metric >= self.best_metric:
+            self.best_metric = metric
             th.save({
                 "epoch": epoch,
                 "model_state_dict": model.state_dict(),
